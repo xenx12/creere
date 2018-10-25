3d5184f8be9c125ba2c4a6fac3353e698b3732f4
LombanoJ <origamilombanoj@gmail.com>
2018-10-25 11:55:30 +0000

    conexion4

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+
+$server = 'us-cdbr-iron-east-01.cleardb.net';
+$username = 'b95929a40c3659';
+$password = 'de9e8f43';
+$db = 'heroku_b79eecc5c4a1f44';
 
 		$mysqli = new mysqli($server, $username, $password, $db);
 		if ($mysqli -> connect_errno) {
